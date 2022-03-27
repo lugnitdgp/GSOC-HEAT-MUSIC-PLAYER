c069ed0679b019a7505fab9d4c24e0029b52d3ae
mayankt18 <mt.20u10405@btech.nitdgp.ac.in>
2022-03-27 18:27:24 +0530

    [ADD] contributing.md

--- spotify/credentials.py
+++ spotify/credentials.py
@@ -1,3 +1,3 @@
-CLIENT_ID = "use your spotify client id here"
-CLIENT_SECRET = "use you spotify client secret here"
+CLIENT_ID = "8fcdab76920f4bf0b2dabb1a16eefd0b"
+CLIENT_SECRET = "b5e81aff83524d929644df369c940be2"
 REDIRECT_URI = "http://127.0.0.1:8000/spotify/redirect"
